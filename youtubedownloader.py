d65a68f3c1af1b81fe0094024e62e4883f20337a
King Igbozuruike <kingobiomaeze2020@gmail.com>
2023-12-15 01:23:13 -0600

    changed background to black, readjusted screen size and elements.

--- youtubedownloader.py
+++ youtubedownloader.py
@@ -2,25 +2,26 @@ from tkinter import *
 from pytube import YouTube
 
 root = Tk()
-root.geometry('500x300')
+root.geometry('1000x600')
 root.resizable(0, 0)
-root.title('youtube downloader')
+root.title('Youtube Downloader')
+root.configure(bg='#202124')
 
 
-Label(root,text='Download Youtube videos for free', font='san-serif 14 bold').pack()
+Label(root,text='Download Youtube videos for free!', font='roboto 20 bold', fg='white', bg='#202124', pady=20).pack()
 
 link = StringVar()
 
-Label(root, text="Paste your link here", font = 'san-serif 15 bold').place(x=150, y=55)
-link_enter = Entry(root, width=70, textvariable=link).place(x=30, y=85)
+Label(root, text="Paste your link here", font = 'san-serif 15 bold', fg='white', bg='#202124').place(x=400, y=150)
+link_enter = Entry(root, width=70, textvariable=link).place(x=300, y=200)
 
 
 def download():
     url = YouTube(str(link.get()))
     video = url.streams.filter(file_extension='mp4', resolution='720p').first()
     video.download()
-    Label(root, text='Downloaded', font='arial 15').place(x=100, y=120)
+    Label(root, text='Downloaded', font='arial 15', fg='white', bg='#202124').place(x=450, y=300)
 
-Button(root, text='Download', font='san-serif 16 bold', bg='red', padx=2, command=download).place(x=100,y=150)
+Button(root, text='Download', font='san-serif 16 bold', bg='red', padx=2, command=download).place(x=450,y=250)
 
 root.mainloop()
